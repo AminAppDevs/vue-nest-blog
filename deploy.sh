7d1614f88296e7ceb9c1f401a9110828b2a63df9
AminAppDevs <90038071+AminAppDevs@users.noreply.github.com>
2022-05-26 07:40:59 +0300

    new

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m "deploy (commit: $COMMIT)"
 
 # if you are using HTTPS/2FA authentication
 # git push -f https://github.com/<USER-NAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/AminAppDevs/vuejs-tailwind-music-player.git master:gh-pages
+git push -f https://github.com/AminAppDevs/vue-nest-blog.git master:gh-pages
 
 cd ..
