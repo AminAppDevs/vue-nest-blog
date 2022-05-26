4be0f81357199d3498fad3ae82ba0360be9b2a06
AminAppDevs <90038071+AminAppDevs@users.noreply.github.com>
2022-05-26 08:15:13 +0300

    deploy (commit: d4b418320d266ee1567b157b269e606f82184a99)

---
+++ js/about.eea4604b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_nest_blog"]=self["webpackChunkvue_nest_blog"]||[]).push([[443],{7381:function(n,e,u){u.r(e),u.d(e,{default:function(){return b}});var t=u(6252);const s={class:"about"},a=(0,t._)("h1",null,"This is an about page",-1),r=[a];function c(n,e){return(0,t.wg)(),(0,t.iD)("div",s,r)}var o=u(3744);const i={},l=(0,o.Z)(i,[["render",c]]);var b=l}}]);
+//# sourceMappingURL=about.eea4604b.js.map
